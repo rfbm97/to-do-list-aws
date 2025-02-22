ed22833d00ec93fc967c77cefbea2ac2bb1b6661
rfbm97 <97549155+rfbm97@users.noreply.github.com>
2025-02-22 17:45:32 +0100

    Update todoApiTest.py for differences between CI and CD

--- test/integration/todoApiTest.py
+++ test/integration/todoApiTest.py
@@ -14,11 +14,14 @@ DEFAULT_TIMEOUT = 2  # in secs
 
 @pytest.mark.api
 class TestApi(unittest.TestCase):
-    
+    @pytest.mark.ci
+    @pytest.mark.cd
     def setUp(self):
         self.assertIsNotNone(BASE_URL, "URL no configurada")
         self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
 
+    @pytest.mark.ci
+    @pytest.mark.cd
     def test_api_listtodos(self):
         print('---------------------------------------')
         print('Starting - integration test List TODO')
@@ -49,6 +52,8 @@ class TestApi(unittest.TestCase):
         self.assertTrue(response.json())
         
         print('End - integration test List TODO')
+
+    @pytest.mark.ci
     def test_api_addtodo(self):
         print('---------------------------------------')
         print('Starting - integration test Add TODO')
@@ -74,6 +79,9 @@ class TestApi(unittest.TestCase):
             response.status_code, 200, "Error en la petición API a {url}"
         )
         print('End - integration test Add TODO')
+
+    @pytest.mark.ci
+    @pytest.mark.cd
     def test_api_gettodo(self):
         print('---------------------------------------')
         print('Starting - integration test Get TODO')
@@ -111,7 +119,8 @@ class TestApi(unittest.TestCase):
             response.status_code, 200, "Error en la petición API a {url}"
         )
         print('End - integration test Get TODO')
-    
+
+    @pytest.mark.ci
     def test_api_updatetodo(self):
         print('---------------------------------------')
         print('Starting - integration test Update TODO')
@@ -165,6 +174,8 @@ class TestApi(unittest.TestCase):
             response.status_code, 200, "Error en la petición API a {url}"
         )
         print('End - integration test Update TODO')
+
+    @pytest.mark.ci
     def test_api_deletetodo(self):
         print('---------------------------------------')
         print('Starting - integration test Delete TODO')
